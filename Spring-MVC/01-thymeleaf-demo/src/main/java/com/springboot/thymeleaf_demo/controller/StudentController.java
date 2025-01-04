bfc21412723766b69ecdaa35d57054eb2e8cd091
Rishib#321 <rishitab2209@gmail.com>
2025-01-04 12:55:17 +0530

    Updated files

--- Spring-MVC/01-thymeleaf-demo/src/main/java/com/springboot/thymeleaf_demo/controller/StudentController.java
+++ Spring-MVC/01-thymeleaf-demo/src/main/java/com/springboot/thymeleaf_demo/controller/StudentController.java
@@ -1,14 +1,20 @@
 package com.springboot.thymeleaf_demo.controller;
 
 import com.springboot.thymeleaf_demo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
@@ -17,6 +23,9 @@ public class StudentController {
 
         // add student object as a model attribute
         theModel.addAttribute("student", theStudent);
+
+        // add the list of countries to the model
+        theModel.addAttribute("countries", countries);
 
         return "student-form";
     }
@@ -27,6 +36,7 @@ public class StudentController {
         // log the input data
         System.out.println("theStudent: " + theStudent.getFirstName() + " " +
                 theStudent.getLastName());
+        System.out.println("Country: " + theStudent.getCountry());
 
         return "student-confirmation";
     }
