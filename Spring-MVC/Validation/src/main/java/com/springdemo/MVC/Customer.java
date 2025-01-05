c24c865ce9effe21004c973cb80d51ca30f2f1b2
Rishib#321 <rishitab2209@gmail.com>
2025-01-05 15:31:30 +0530

    Spring Validation

--- Spring-MVC/Validation/src/main/java/com/springdemo/MVC/Customer.java
+++ Spring-MVC/Validation/src/main/java/com/springdemo/MVC/Customer.java
@@ -1,7 +1,6 @@
 package com.springdemo.MVC;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -11,6 +10,13 @@ public class Customer {
     @Size(min=1, message="is required")
     private String lastName = "";
     // initialized to null
+
+    @Min(value=0, message="must be greater than or equal to zero")
+    @Max(value=10, message="must be less than or equal to 10")
+    private int freePasses;
+
+    @Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -27,5 +33,22 @@ public class Customer {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
 
 }
