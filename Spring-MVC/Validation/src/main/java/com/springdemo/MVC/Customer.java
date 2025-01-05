1e3905c2bf5e5dbf93150594333e0a771e4a39d2
Rishib#321 <rishitab2209@gmail.com>
2025-01-05 15:51:07 +0530

    Spring Validation

--- Spring-MVC/Validation/src/main/java/com/springdemo/MVC/Customer.java
+++ Spring-MVC/Validation/src/main/java/com/springdemo/MVC/Customer.java
@@ -13,7 +13,11 @@ public class Customer {
 
     @Min(value=0, message="must be greater than or equal to zero")
     @Max(value=10, message="must be less than or equal to 10")
-    private int freePasses;
+    @NotNull(message="is required")
+    private Integer freePasses;
+
+   // private int freePasses;
+    // use Integer for string to int
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
@@ -34,11 +38,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
