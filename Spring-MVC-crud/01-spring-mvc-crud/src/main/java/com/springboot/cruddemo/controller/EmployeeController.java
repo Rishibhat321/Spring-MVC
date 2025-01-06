2745c9e78cc479fd9941cf0c4029778778720950
Rishib#321 <rishitab2209@gmail.com>
2025-01-06 19:58:59 +0530

    Created employee controller

--- Spring-MVC-crud/01-spring-mvc-crud/src/main/java/com/springboot/cruddemo/controller/EmployeeController.java
+++ Spring-MVC-crud/01-spring-mvc-crud/src/main/java/com/springboot/cruddemo/controller/EmployeeController.java
@@ -1,4 +1,37 @@
 package com.springboot.cruddemo.controller;
 
+import com.springboot.cruddemo.entity.Employee;
+import com.springboot.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
 public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+
+        // get the employees from db
+        List<Employee> theEmployees = employeeService.findAll();
+
+        // add to the spring model
+        theModel.addAttribute("employees", theEmployees);
+
+        return "list-employee";
+    }
+
+
 }
