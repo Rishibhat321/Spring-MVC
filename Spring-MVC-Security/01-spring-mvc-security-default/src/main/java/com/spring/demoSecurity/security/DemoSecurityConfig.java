ec2d3d559949faa9b205be5fd028a8396ef06158
Rishib#321 <rishitab2209@gmail.com>
2025-01-27 13:27:28 +0530

    spring-mvc-security

--- Spring-MVC-Security/01-spring-mvc-security-default/src/main/java/com/spring/demoSecurity/security/DemoSecurityConfig.java
+++ Spring-MVC-Security/01-spring-mvc-security-default/src/main/java/com/spring/demoSecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.spring.demoSecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -37,4 +39,23 @@ public class DemoSecurityConfig {
         return new InMemoryUserDetailsManager(john, mary, susan);
 
     }
+
+    // configure security of web apps
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .anyRequest().authenticated()
+        )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                );
+
+        return http.build();
+    }
+
 }
