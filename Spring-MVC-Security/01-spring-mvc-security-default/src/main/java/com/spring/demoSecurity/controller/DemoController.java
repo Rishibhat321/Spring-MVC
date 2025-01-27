97b0d4e606af0c5b435bced4338ec6914199222f
Rishib#321 <rishitab2209@gmail.com>
2025-01-27 13:19:22 +0530

    spring-mvc-security

--- Spring-MVC-Security/01-spring-mvc-security-default/src/main/java/com/spring/demoSecurity/controller/DemoController.java
+++ Spring-MVC-Security/01-spring-mvc-security-default/src/main/java/com/spring/demoSecurity/controller/DemoController.java
@@ -11,4 +11,10 @@ public class DemoController {
         return "home";
     }
 
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage() {
+        return "plain-login";
+    }
+
+
 }
