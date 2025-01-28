ec105fae2bfc36d84c0715357cafc6fc4e8270ba
Rishib#321 <rishitab2209@gmail.com>
2025-01-28 13:19:09 +0530

    spring mvc security

--- Spring-MVC-Security/01-spring-mvc-security-default/src/main/java/com/spring/demoSecurity/controller/DemoController.java
+++ Spring-MVC-Security/01-spring-mvc-security-default/src/main/java/com/spring/demoSecurity/controller/DemoController.java
@@ -19,6 +19,12 @@ public class DemoController {
         return "leaders";
     }
 
+    // add request mapping for /systems
+    @GetMapping("/systems")
+    public String showSystems() {
+
+        return "systems";
+    }
 
 
 }
